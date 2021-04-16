1e866810a9f0b874729a2060e86199c439745d66
LS <latifsulistyo.me@gmail.com>
2021-04-16 15:05:49 +0700

    refactor: rewrite to JS for modulation

---
+++ src/themes/colors.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const text = {
+  chalky: '#e5c07b',
+  coral: '#ef596f',
+  dark: '#5c6370',
+  error: '#f44747',
+  fountainBlue: '#2bbac5',
+  green: '#89ca78',
+  invalid: '#ffffff',
+  lightDark: '#7f848e',
+  lightWhite: '#abb2bf',
+  malibu: '#61afef',
+  purple: '#d55fde',
+  whiskey: '#d19a66',
+  deepRed: '#BE5046',
+};
+
+const theme = {
+  // base
+  background: '#282A36',
+  foreground: '#F8F8F2',
+  selection: '#44475A',
+  comment: '#6272A4',
+  cyan: '#8BE9FD',
+  green: '#50FA7B',
+  orange: '#FFB86C',
+  pink: '#FF79C6',
+  purple: '#BD93F9',
+  red: '#FF5555',
+  yellow: '#F1FA8C',
+
+  // ansi
+  color0: '#2d3139',
+  color1: '#e06c75',
+  color2: '#98c379',
+  color3: '#e5c07b',
+  color4: '#61afef',
+  color5: '#c678dd',
+  color6: '#56b6c2',
+  color7: '#d7dae0',
+  color8: '#7f848e',
+  color9: '#f44747',
+  color10: '#98c379',
+  color11: '#e5c07b',
+  color12: '#528bff',
+  color13: '#7e0097',
+  color14: '#56b6c2',
+  color15: '#d7dae0',
+
+  // other
+  lineHighlight: '#44475A75',
+  nonText: '#FFFFFF1A',
+  white: '#ffffff',
+  tabDropBackground: '#44475A70',
+
+  // UI variants
+  backgroundLighter: '#424450',
+  backgroundLight: '#343746',
+  backgroundDark: '#21222C',
+  backgroundDarker: '#191A21',
+};
+
+module.exports = { text, theme };
